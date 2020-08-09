4a50b180f1a8f9d6b066b93e1cf053f74b5f2bc3
Grzegorz Czmielewski <grzegorz.czmielewski@etteplan.com>
2020-08-09 14:20:01 +0200

    adjusted manifest and index.html

--- src/__testData/mediaInfo.js
+++ src/__testData/mediaInfo.js
@@ -2,14 +2,14 @@ export default {
   status: 200,
   data: {
     MediaId: 15,
-    Title: "Top Gun \nMAVERICK",
+    Title: "Some example",
     Description:
-      "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing \nthe envelope as a courageous test pilot and dodging\nthe advancement in rank that would ground him.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     MediaTypeCode: "VOD",
     MediaTypeDisplayName: "VOD",
     StreamId: 0,
     Provider: "Internal",
     ContentUrl:
-      "https://cd-streamod.telenorcdn.net/tnfbaod/SF/585db4b3e4b09db0cf348a64/dash_a1.ism/playlist.mpd",
+      "https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd",
   },
 };
