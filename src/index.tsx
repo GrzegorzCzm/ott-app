57c6afad559d8f9fc871e3c89b3a8ff90157e783
Grzegorz Czmielewski <grzegorz.czmielewski@etteplan.com>
2021-03-03 14:42:51 +0100

    added redux devtools

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,22 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import reducers from "./redux/reducers";
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    // @ts-ignore
+    window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"]) ||
+  compose;
+
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
